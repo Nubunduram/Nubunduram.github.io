be35b4e0d4e32b71b0dc81367721a50e3e0a88ef
Nubunduram <ceantoine@hotmail.fr>
2024-04-15 18:11:07 +0200

    feat: add link to projects

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -8,6 +8,7 @@ interface ProjectData {
   details: string;
   stack: string[];
   imageUrl: string;
+  link: string;
 }
 
 const Projects: React.FC = () => {
@@ -16,19 +17,24 @@ const Projects: React.FC = () => {
       title: 'Project Kasa',
       details: 'Kasa Project is a front-end web application developped using Scss & React. The project display accomodations pictures, details, ratings, ... All of it coming from a JSON file.',
       stack: ['React', 'Scss'],
-      imageUrl: './assets/kasa.png'
+      imageUrl: './assets/kasa.png',
+      link: "https://nubunduram.github.io/Kasa/",
+
     },
     {
       title: 'Architect Portfolio',
       details: 'For this project i had to implement a Login page and an editor mode via modal to connect with the back-end and update the DataBase',
       stack: ['HTML', 'CSS', 'JavaScript'],
-      imageUrl: './assets/portfolio-sophie.png'
+      imageUrl: './assets/portfolio-sophie.png',
+      link: "https://nubunduram.github.io/Kasa/",
+
     },
     {
       title: 'Book Rating Site',
       details: 'Creating the server for the website, CRUD, Ratings Logic, Green IT, authentification with JWT & bcrypt, MVC',
       stack: ['Node.js', 'Express', 'MongoDB'],
-      imageUrl: './assets/grimoire.png'
+      imageUrl: './assets/grimoire.png',
+      link: "https://nubunduram.github.io/Kasa/",
     }
   ];
 
@@ -37,7 +43,7 @@ const Projects: React.FC = () => {
       <h2 className="projects-section__title">Projects</h2>
       <div className="projects-grid">
         {projectsData.map((project, index) => (
-          <a key={index} href="#" target="_blank" className={index === projectsData.length - 1 ? 'last-project' : ''}>
+          <a key={index} href={project.link} target="_blank" className={index === projectsData.length - 1 ? 'last-project' : ''}>
             <img src={project.imageUrl} alt={`Project ${index + 1}`} />
             <Project project={project} />
           </a>
