cb6fc9356cab7d051ee813b1b82461e3377a4ff8
Nubunduram <ceantoine@hotmail.fr>
2024-04-26 16:26:08 +0200

    fix: changed details in paragraphs

--- src/components/Home/About/About.tsx
+++ src/components/Home/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
   const paragraphs = [
     "👋 A passionate and self-taught web developer. Over the past years, I discovered the world of web development, learning from various classes, professionals, and my own project experiences. Before joining a class to get a diploma.",
     "🚀 My journey involves working closely with a talented web designer friend, where I take his creative concepts and transform them into functional websites. This collaborative challenge is incredibly fun and enriching, offering me the opportunity to continuously learn and refine my skills.",
-    "💡 I focus on front-end technologies, HTML, CSS, JS, React, TypeScript, Scss, and more. But I want to learn more from the tech i already know and also learn new tech like Vue, Angular, Docker, Back-end Technologies...",
+    "💡 My favorite front-end technologies are React and Sass. And for the back-end it would be Node.JS, Express & MongoDB. But I also love learning new technologies !",
     "🌍 Originally from France, I am fluent in both English and French, allowing me to comfortably communicate and work in either language. I also have basis in German and am wheeling to learn more.",
     "🔍 While my professional journey has seen me in diverse roles from restaurant management to brewing, I've found my true passion in web development. Guided by the principle of delivering the best possible results, I approach each project with a commitment to excellence.",
     "🤝 Currently benefiting from the mentorship of a web designer and a software engineer, each with over five years of industry experience, I'm continually learning. Together, we're building a collaborative space where knowledge flows freely, and each challenge becomes an opportunity for growth.",
