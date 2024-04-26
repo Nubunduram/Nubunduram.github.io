a271f1882d43cdfee5318e038490ef0a365f396a
Nubunduram <ceantoine@hotmail.fr>
2024-04-26 16:11:23 +0200

    fix: Quote

--- src/components/Home/Quote/Quote.tsx
+++ src/components/Home/Quote/Quote.tsx
@@ -8,7 +8,7 @@ const Quote: React.FC<QuoteProps> = ({ quote }) => {
   return (
     <section className="quote-section">
       <p>
-        <q><i className="fa fa-quote-left"></i>{quote}</q>
+        <q><i className="fa fa-quote-left"></i>{quote}<i className="fa fa-quote-right"></i></q>
       </p>
     </section>
   );
