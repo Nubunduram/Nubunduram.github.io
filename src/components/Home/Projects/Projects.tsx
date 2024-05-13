353bbe981e81c89d60704d89bce114f443d186a2
Nubunduram <ceantoine@hotmail.fr>
2024-05-13 18:43:56 +0200

    feat: Added Webp for all images

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -1,13 +1,14 @@
 // Projects.tsx
 import React from 'react';
-import "./_Projects.scss";
-import Project from "./Project/Project";
+import './_Projects.scss';
+import Project from './Project/Project';
 
 interface ProjectData {
   title: string;
   details: string;
   stack: string[];
-  imageUrl: string;
+  imageUrlWebP: string;
+  imageUrlFallback: string;
   link: string;
 }
 
@@ -15,27 +16,31 @@ const Projects: React.FC = () => {
   const projectsData: ProjectData[] = [
     {
       title: 'Project Kasa',
-      details: 'Kasa is a front-end web application developped using Scss & React. The project display accomodations pictures, details, ratings, ... All of it coming from a JSON file.',
+      details:
+        'Kasa is a front-end web application developed using Scss & React. The project displays accommodation pictures, details, ratings, ... All of it coming from a JSON file.',
       stack: ['React', 'Sass'],
-      imageUrl: './assets/kasa.png',
-      link: "https://nubunduram.github.io/Kasa/",
-
+      imageUrlWebP: './assets/kasa.webp',
+      imageUrlFallback: './assets/kasa.png',
+      link: 'https://nubunduram.github.io/Kasa/',
     },
     {
       title: 'Architect Portfolio',
-      details: 'For this project i had to implement a Login page and an editor mode via modal to connect with the back-end and update the DataBase',
+      details:
+        'For this project, I had to implement a Login page and an editor mode via modal to connect with the backend and update the DataBase',
       stack: ['HTML', 'CSS', 'JavaScript'],
-      imageUrl: './assets/portfolio-sophie.png',
-      link: "https://nubunduram.github.io/portfolio-architecte-sophie-bluel/",
-
+      imageUrlWebP: './assets/portfolio-sophie.webp',
+      imageUrlFallback: './assets/portfolio-sophie.png',
+      link: 'https://nubunduram.github.io/portfolio-architecte-sophie-bluel/',
     },
     {
       title: 'Book Rating Site',
-      details: 'Creating the server for the website, CRUD, Ratings Logic, Green IT, authentification with JWT & bcrypt, MVC',
+      details:
+        'Creating the server for the website, CRUD, Ratings Logic, Green IT, authentication with JWT & bcrypt, MVC',
       stack: ['Node.js', 'Express', 'MongoDB'],
-      imageUrl: './assets/grimoire.png',
-      link: "https://nubunduram.github.io/Grimoire/",
-    }
+      imageUrlWebP: './assets/grimoire.webp',
+      imageUrlFallback: './assets/grimoire.png',
+      link: 'https://nubunduram.github.io/Grimoire/',
+    },
   ];
 
   return (
@@ -45,14 +50,22 @@ const Projects: React.FC = () => {
       </header>
       <div className="projects-grid">
         {projectsData.map((project, index) => (
-          <a key={index} href={project.link} target="_blank" className={index === projectsData.length - 1 ? 'last-project' : ''}>
-            <img src={project.imageUrl} alt={`Project ${index + 1}`} />
+          <a
+            key={index}
+            href={project.link}
+            target="_blank"
+            className={index === projectsData.length - 1 ? 'last-project' : ''}
+          >
+            <picture>
+              <source srcSet={project.imageUrlWebP} type="image/webp" />
+              <img src={project.imageUrlFallback} alt={`Project ${index + 1}`} />
+            </picture>
             <Project project={project} />
           </a>
         ))}
       </div>
     </section>
   );
-}
+};
 
 export default Projects;
